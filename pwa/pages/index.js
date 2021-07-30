88cda1a871f43e2db4c2f5768cfe09adbde9caa1
HamzaKarfa <hamza.karfa@gmail.com>
2021-07-30 09:22:46 +0200

    push main PR master pull on server

--- pwa/pages/index.js
+++ pwa/pages/index.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div>
       <Link href="/">
-      	<h1>test pr on branch main 1</h1>
+      	<h1>test pr on branch main 2</h1>
       </Link>
     </div>
   )
